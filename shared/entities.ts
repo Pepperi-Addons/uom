a099b845d19bcb089802531c9e6f0720d5cc023a
roiaharon-pepperi <66308169+roiaharon-pepperi@users.noreply.github.com>
2021-04-29 14:33:13 +0300

    entities naming changes

--- shared/entities.ts
+++ shared/entities.ts
@@ -3,21 +3,20 @@ export const CPI_NODE_ADDON_UUID = 'bb6ee826-1c6b-4a11-9758-40a46acb69c5';
 
 export interface Uom {
     Key: string,
-    Name: string,
+    Title: string,
     Multiplier: number,
-    BaseUOM: string,
     Hidden?: boolean
 }
 
 export interface AtdConfiguration {
-    Key: string, // Id of the atd this configuration belongs to
-    AllowedUomMapping: string,
-    InventoryMapping: string,
-    InventoryAction: InventoryAction
+    Key: string, 
+    UOMFieldID: string,
+    InventoryFieldID: string,
+    InventoryType: string
 }
 
 export enum InventoryAction {
-    DoNothing = 'Uom_AtdConfig_InventoryAction_DoNothingOption',
-    Correct = 'Uom_AtdConfig_InventoryAction_CorrectOption',
-    Color = 'Uom_AtdConfig_InventoryAction_ColorOption',
+    DoNothing = 'Do Nothing',
+    Fix = 'Fix Inventory',
+    Color = 'Color',
 }
