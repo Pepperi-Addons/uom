3757ee63019c8f85db3e34a08bc6210202f1f7b9
michaelatias <87411559+michaelatias@users.noreply.github.com>
2021-08-09 17:51:39 +0300

    last commit before QA

--- cpi-side/uom-app.ts
+++ cpi-side/uom-app.ts
@@ -312,9 +312,9 @@ class UOMManager {
         const config = itemConfig.find(item => item.UOMKey === uom.Key);
         return {
             UOMKey: uom.Key,
-            Factor: config? (Number(config.Factor) == 0? 1: Number(config.Factor))  || uom.Multiplier: uom.Multiplier,
-            Min: config? Number(config.Min): 0,
-            Case: config? Number(config.Case) == 0? 1: Number(config.Case) :1
+            Factor: (config != undefined && config.Factor!= undefined)? (Number(config.Factor) == 0? 1: Number(config.Factor))  || uom.Multiplier: uom.Multiplier,
+            Min: (config != undefined && config.Min != undefined)? Number(config.Min): 0,
+            Case: (config != undefined && config.Case != undefined)? Number(config.Case) == 0? 1: Number(config.Case) :1
         }; 
     }
     getUomMinQuantity(uom: Uom | undefined, itemConfig: UomItemConfiguration[]) : number{
