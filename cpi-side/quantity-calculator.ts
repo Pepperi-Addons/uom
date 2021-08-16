59dc79dcbcfa7f29b972d83b4e2ffd3ce61cb682
michaelatias <87411559+michaelatias@users.noreply.github.com>
2021-08-16 10:40:14 +0300

    commit

--- cpi-side/quantity-calculator.ts
+++ cpi-side/quantity-calculator.ts
@@ -121,10 +121,30 @@ export class QuantityCalculator {
                 return this.fix(Math.max(num,0),ItemAction.Set);
             }
             fix(num: number, action: ItemAction){
+                //first shift left everything by decimal
+                num = this.convertToInteger(num);
                 let res = this.fixByCase(num,action);
                 res = this.fixByMin(res,action);
                 res = this.fixByMax(res, action);
-                return this.resultBuilder(res);
+                //shift right back to the original base by decimal
+                res = this.convertToDec(res);
+                return this.resultBuilder(res)
+            }
+            //in order to support frac we just sfhit left $decimal digits, and then work on integers
+            convertToInteger(num:number):number{
+                let shifter = Math.pow(10,this.decimal);
+                this.originalMin = this.originalMin * shifter;
+                this.cq = this.cq * shifter;
+                this.normalizedInv = this.normalizedInv * shifter;
+                return num * shifter;
+            }
+            // here we shift right to go back to the original base of the number.
+            convertToDec(res: number):number{
+                let shifter = Math.pow(10,this.decimal);
+                this.originalMin = Number((this.originalMin / shifter).toFixed(this.decimal));
+                this.cq = Number((this.cq / shifter).toFixed(this.decimal));
+                this.normalizedInv = Number((this.normalizedInv / shifter).toFixed(this.decimal));
+                return Number((res/shifter).toFixed(this.decimal));
             }
 }
 
