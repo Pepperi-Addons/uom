8501d29cf3d2962d7adf5035d4da692f238773e1
michaelatias <87411559+michaelatias@users.noreply.github.com>
2021-08-29 17:40:41 +0300

    fixed TSA to be real number and not decimal

--- server-side/metadata.ts
+++ server-side/metadata.ts
@@ -88,7 +88,7 @@ export const UomTSAFields: ApiFieldObject[] = [
         Description: "The quantity for the 1st unit of measure",
         IsUserDefinedField: true,
         UIType: {
-            ID: 28,
+            ID: 29,
             Name: "NumberIntegerQuantitySelector",
         },
         Type: "Integer",
@@ -100,7 +100,7 @@ export const UomTSAFields: ApiFieldObject[] = [
         Description: "The quantity for the 2nd unit of measure",
         IsUserDefinedField: true,
         UIType: {
-            ID: 28,
+            ID: 29,
             Name: "NumberIntegerQuantitySelector"
         },
         Type: "Integer",
