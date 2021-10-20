e99b4688431bea996411328fd1c0441d6fda18c8
michaelatias <87411559+michaelatias@users.noreply.github.com>
2021-10-20 16:28:04 +0300

    pr commit

--- client-side/src/app/components/atd-config/atd-config.module.ts
+++ client-side/src/app/components/atd-config/atd-config.module.ts
@@ -14,7 +14,6 @@ import { AtdConfigService } from './atd-config.service';
     declarations: [
         AtdConfigComponent,
         InstallationComponent,
-        
     ],
     imports: [
         CommonModule,
